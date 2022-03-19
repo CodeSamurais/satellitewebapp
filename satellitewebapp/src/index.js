dfe49ee70fd54bdbe467b5edf7accac296980b76
StealtyPanda <101936743+CodeSamurais@users.noreply.github.com>
2022-03-19 21:29:41 +0400

    Initialized the react app and the project

---
+++ satellitewebapp/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+
+
+
+
+
+
+
+
+
+ReactDOM.render( <p>Test to see site is up and running</p> , document.getElementById('root'));
