159f40ee69ba6181f4548b9e634d27a6749e8d24
StealtyPanda <shaikm259@gmail.com>
2022-04-16 20:46:40 +0400

    I added parity indicator
    Facebook better watch out.
    Amazon and google shaking and crying right now.

--- satellitewebapp/src/sidebar.js
+++ satellitewebapp/src/sidebar.js
@@ -5,11 +5,13 @@ import "./sidebar.css";
 export function SideBar(props)
 {
     const [value, setValue] = useState(0);
+    const [iseven, setIsEven] = useState(true);
 
     return (
         <div className='clicker' onClick={() => {
             setValue(value + 1);
-        }}>Test value is {value}</div>
+            setIsEven((value + 1) % 2 == 0);
+        }}>Test value is {value}. Is it even? {`${iseven}`}</div>
     );
 }
 
